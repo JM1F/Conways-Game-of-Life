059a04c7a719acf8b13d6cb682886c3667076950
JM1F <joe7business@gmail.com>
2021-07-14 01:27:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Conway_s_Game_of_Life
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
