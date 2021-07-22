bbfb3ad8ec85a589e288cd09665e4b3bc19b4e7f
JM1F <joe7business@gmail.com>
2021-07-22 17:54:12 +0100

    Added and made a title with the function to toggle.

--- Game1.cs
+++ Game1.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System.Diagnostics;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Conways_Game_of_Life
 {
@@ -12,9 +8,16 @@ namespace Conways_Game_of_Life
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private SpriteBatch _spriteBatchLOGO;
         private Grid _grid;
         public View CurrentView;
         public Cell[,] GridList;
+        public Rectangle LogoRectangle;
+        public bool Logohovered { get; set; } = false;
+        public bool Logoclicked { get; set; } = false;
+
+        public Texture2D Logo;
+        public KeyboardState PreviousKeyboardstate;
         
 
         public Game1()
@@ -23,10 +26,10 @@ namespace Conways_Game_of_Life
 
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            this.Window.AllowUserResizing = true;
+            this.Window.AllowUserResizing = false;
 
         }
-
+        
         protected override void Initialize()
         {
 
@@ -48,23 +51,72 @@ namespace Conways_Game_of_Life
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatchLOGO = new SpriteBatch(GraphicsDevice);
+            Logo = this.Content.Load<Texture2D>("GOLLogo");
+            LogoRectangle = new Rectangle(490, 0, 300, 125);
         }
 
         protected override void Update(GameTime gameTime)
         {
+
+            var mouseState = Mouse.GetState();
+            var mouseLocation = new Point(mouseState.X, mouseState.Y);
+
+            var keyboardState = Keyboard.GetState();
+
             CurrentView.Update(gameTime, GraphicsDevice.Viewport, Window);
             
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            if (keyboardState.IsKeyDown(Keys.T) && PreviousKeyboardstate.IsKeyUp(Keys.T))
+            {
+                Logoclicked = !Logoclicked;
+            }
+
+
+            if (LogoRectangle.Contains(mouseLocation))
+            {
+                Logohovered = true;
+            }
+            else 
+            {
+                Logohovered = false;    
+            }
+            
 
             _grid.Update();
             base.Update(gameTime);
+
+            PreviousKeyboardstate = keyboardState;
         }
 
         protected override void Draw(GameTime gameTime)
         {
+            
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _grid.Draw(_spriteBatch);
+
+            _spriteBatchLOGO.Begin();
+           
+            if (Logoclicked == false)
+            {
+                if (Logohovered == true)
+                {
+                    _spriteBatchLOGO.Draw(Logo, LogoRectangle, new Color(Color.White, 0.9f));
+                }
+                else
+                {
+                    _spriteBatchLOGO.Draw(Logo, LogoRectangle, new Color(Color.White, 0.7f));
+                }
+            }
+            
+
+            
+            
+            
+
+            _spriteBatchLOGO.End();
+
             base.Draw(gameTime);
         }
     }
